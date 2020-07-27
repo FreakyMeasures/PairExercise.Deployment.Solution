471d815e94782e37d1694db072d729ff3cddf261
Jake Armijo <armijojake@yahoo.com>
2020-07-27 14:13:47 -0500

    This is a simple commit

--- client/components/UserList.jsx
+++ client/components/UserList.jsx
@@ -1,15 +1,20 @@
-import React from 'react'
-import User from './User.jsx'
+import React from "react";
+import User from "./User.jsx";
 
 export default ({ users }) => (
   <div className="user-list">
+    <h2>THIS IS A TESTer</h2>
 
-    <p className='lead'>Here's a list of {users.length} completely fictitious people:</p>
+    <p className="lead">
+      Here's a list of {users.length} completely fictitious people:
+    </p>
 
     <table className="table">
       <tbody>
-        { users.map(u => <User {...u} key={u.id} />) }
+        {users.map((u) => (
+          <User {...u} key={u.id} />
+        ))}
       </tbody>
     </table>
   </div>
-)
+);
